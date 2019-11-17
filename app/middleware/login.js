08e7e78f339258b755c8501d5fffbcb5c762c3d4
jiguangchao <253106788@qq.com>
2019-11-17 17:46:47 +0800

    rename auth to login

---
+++ app/middleware/login.js
@@ -0,0 +1,11 @@
+module.exports = () => {
+    return async function login(ctx, next) {
+        // console.log('in auth middleware');
+        if (ctx.isAuthenticated()) {
+            console.log('in login', JSON.stringify(ctx.user, null, 2));
+            await next();
+        } else {
+            ctx.redirect('/loginPage');
+        }
+    }
+};
